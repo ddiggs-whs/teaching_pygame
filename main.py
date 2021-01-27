5075698b0214418e44fb90db31711bcc49b0501e
Daniel Diggs <ddiggs@whs.bucks.sch.uk>
2021-01-27 11:44:40 +0000

    First changes

--- main.py
+++ main.py
@@ -1,22 +1,76 @@
 import pygame
 
-
 pygame.init()
 
+WINDOWWIDTH = 800
+WINDOWHEIGHT = 600
 
-DISPLAYSURFACE = pygame.display.set_mode([800, 600])
+DISPLAY = pygame.display.set_mode([WINDOWWIDTH, WINDOWHEIGHT])
 FPSCLOCK = pygame.time.Clock()
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+left = 50
+top = 50
+width = 40
+height = 40
+x_speed = 5
+y_speed = 5
+x_color = RED
+mos_pos = (0, 0)
+
+
+class BouncingRectangle(pygame.sprite.Sprite):
+
+    def __init__(self, width, height):
+        super().__init__()
+        self.image = pygame.Surface([width, height])
+        self.rect = self.image.get_rect()
+        self.x_color = RED
+
+        self.image.fill(WHITE)
+        pygame.draw.line(self.image, self.x_color, self.rect.topleft, self.rect.bottomright, 10)
+        pygame.draw.line(self.image, self.x_color, self.rect.bottomleft, self.rect.topright, 10)
+
+        self.x_speed = 5
+        self.y_speed = 5
+
+    def update(self):
+        self.rect.left += self.x_speed
+        if self.rect.right >= WINDOWWIDTH:
+            self.x_speed *= -1
+        if self.rect.left <= 0:
+            self.x_speed *= -1
+
+        self.rect.top += self.y_speed
+        if self.rect.bottom >= WINDOWHEIGHT:
+            self.y_speed *= -1
+        if self.rect.top <= 0:
+            self.y_speed *= -1
+
+my_rectangle = BouncingRectangle(80, 80)
+my_group = pygame.sprite.Group()
+my_group.add(my_rectangle)
+
 
 while True:
 
     for event in pygame.event.get():
+        if event.type == pygame.MOUSEMOTION:
+            mos_pos = pygame.mouse.get_pos()
+            if left < mos_pos[0] < left + width and top < mos_pos[1] < top + height:
+                x_color = GREEN
+            else:
+                x_color = RED
         if event.type == pygame.QUIT:
             quit()
 
-    DISPLAYSURFACE.fill((255, 0, 0))
+    DISPLAY.fill(BLACK)
 
-    pygame.draw.circle(DISPLAYSURFACE, (0, 0, 0), (300, 300), 20)
+    my_group.update()
+    my_group.draw(DISPLAY)
+
 
     pygame.display.update()
-
     FPSCLOCK.tick(30)
